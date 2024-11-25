232aa005f4e682f1e70210d1feae41a53ff01767
bluemin <zeem_m2@naver.com>
2024-11-25 20:38:58 +0900

    image & video 사이즈 300 x 400으로 고정

--- src/components/Basic/myFeed/AddFeed.tsx
+++ src/components/Basic/myFeed/AddFeed.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ChooseLevel from './ChooseLevel';
 import { useNavigate } from 'react-router-dom';
+import FileUploadButton from './FileUploadButton';
 
 const AddFeed: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
@@ -27,11 +28,6 @@ const AddFeed: React.FC = () => {
     navigate(-1);
   };
 
-  // const handleSubmit = (e: React.FormEvent): void => {
-  //   e.preventDefault();
-  //   console.log('폼 제출 방지됨!');
-  // };
-
   console.log(selectedFile);
 
   return (
@@ -41,18 +37,18 @@ const AddFeed: React.FC = () => {
         <div className="flex flex-col gap-4">
           {selectedFileUrl ? (
             fileType === 'image' ? (
-              <div className="w-[300px] h-[300px] bg-gray-200 rounded-2xl flex flex-col gap-3 items-center justify-center">
+              <div className="w-[300px] h-[400px] bg-gray-200 rounded-2xl flex flex-col gap-3 items-center justify-center">
                 <img src={selectedFileUrl} alt="업로드 이미지" className="w-full h-full object-cover rounded-2xl" />
               </div>
             ) : (
-              <div className="max-w-[300px] min-h-[300px] bg-gray-200 rounded-2xl flex flex-col gap-3 items-center justify-center">
+              <div className="relative min-w-[300px] min-h-[400px] bg-gray-200 rounded-2xl flex flex-col gap-3 items-center justify-center">
                 <video
                   src={selectedFileUrl}
                   controls
                   muted
                   autoPlay
                   loop
-                  className="aspect-w-16 aspect-h-9 rounded-2xl p-0"
+                  className="absolute top-[50%] left-[50%] w-full h-full object-cover transition-transform -translate-x-1/2 -translate-y-1/2 rounded-2xl p-0"
                 />
               </div>
             )
@@ -62,28 +58,8 @@ const AddFeed: React.FC = () => {
             </div>
           )}
 
-          {/* 업로드 아이콘 */}
-          <label
-            htmlFor="fileInput"
-            className="w-[50px] h-[50px] bg-gray-100 rounded-xl flex items-center justify-center cursor-pointer hover:ring-black hover:ring-1 hover:bg-white">
-            <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="12.5" cy="12.5" r="12.5" fill="black" />
-              <path
-                d="M7.37036 13.4087L6.76815 12.8065C6.51316 12.5515 6.51316 12.1392 6.76815 11.8869L12.0389 6.61348C12.2939 6.35849 12.7062 6.35849 12.9585 6.61348L18.2292 11.8842C18.4842 12.1392 18.4842 12.5515 18.2292 12.8038L17.627 13.406C17.3693 13.6637 16.9488 13.6583 16.6965 13.3952L13.5851 10.1291V17.9253C13.5851 18.2861 13.2948 18.5764 12.9341 18.5764H12.066C11.7052 18.5764 11.415 18.2861 11.415 17.9253V10.1291L8.30081 13.3979C8.04853 13.6637 7.62807 13.6691 7.37036 13.4087Z"
-                fill="white"
-              />
-            </svg>
-            {/* <p className="font-noto text-sm">사진을 추가해 주세요.</p> */}
-          </label>
-          {/* 실제 파일 선택 input (숨김 처리) */}
-
-          <input
-            id="fileInput"
-            type="file"
-            style={{ display: 'none' }}
-            onChange={handleFileChange}
-            accept="video/*, image/*"
-          />
+          {/* 파일 업로드 버튼 컴포넌트 사용 */}
+          <FileUploadButton onFileChange={handleFileChange} />
         </div>
 
         {/* form 요소들 */}
