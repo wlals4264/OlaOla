9d5e94e947aa91e4a3c8b5015cc4ef60d1a892ae
bluemin <zeem_m2@naver.com>
2024-11-25 23:55:39 +0900

    firebaseConfig 수정(보안 및 필요없는 함수 삭제)

--- src/Firebase/firebase.ts
+++ src/Firebase/firebase.ts
@@ -1,19 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBB08bs5pR84Iazh5H__hD4h0wbdJ6vq4A',
-  authDomain: 'olaola-b097f.firebaseapp.com',
-  projectId: 'olaola-b097f',
-  storageBucket: 'olaola-b097f.firebasestorage.app',
-  messagingSenderId: '632468279095',
-  appId: '1:632468279095:web:db440074f01b1bc099d4b2',
-  measurementId: 'G-EBST0R5Q8D',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
 
-export { auth, analytics };
+export { auth };
